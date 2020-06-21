65e0706f49c5684eb3c15f850039e4e615713378
admin <1246300978>
2020-06-21 18:05:42 +0800

    文件上传

--- src/main/java/com/webservice/his/ba/eventlisteners/listeners/BlackListNotifier.java
+++ src/main/java/com/webservice/his/ba/eventlisteners/listeners/BlackListNotifier.java
@@ -13,7 +13,7 @@ public class BlackListNotifier{
     @EventListener
     public void aaaa(BlackListEvent event) {
 //        System.out.println("我是【Notifier】正在监听事件【BlackList】");
-//        System.out.println(event);
+        System.out.println(event);
         System.out.println(this);
         // notify appropriate parties via notificationAddress...
 //        System.out.println(event.getAddress());
